5bfbe46373fddfe0a2918b773c4252252d6a3c5d
Balu Thota <balu_thota@homedepot.com>
2018-09-08 09:48:32 -0400

    added validation

--- src/main/java/com/thota/example/CoinApp.java
+++ src/main/java/com/thota/example/CoinApp.java
@@ -1,5 +1,6 @@
 package com.thota.example;
 
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -13,24 +14,31 @@ public class CoinApp
         CoinExchange coinExchange = new CoinExchangeImpl();
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Enter amount : ");
-        int amount = reader.nextInt();
-        while (amount > 0) {
-            Map<String, Integer> denomMap = coinExchange.exchangeCoinsForAmount(amount);
-            if (denomMap == null) {
-                System.out.println(" There are not enough coins to dispense the change, exiting the system");
-                System.exit(0);
-            } else {
-                System.out.printf("Coin Exchange For Amount $%d ==> %s " , amount , denomMap.entrySet()
-                        .stream()
-                        .map(Object::toString)
-                        .collect(joining(" & ")));
-                System.out.println();
-            }
-            reader = new Scanner(System.in);  // Reading from System.in
-            System.out.println("Enter amount : ");
-            amount = reader.nextInt();
+        try {
+            int amount = reader.nextInt();
+            while (amount > 0) {
+                Map<String, Integer> denomMap = coinExchange.exchangeCoinsForAmount(amount);
+                if (denomMap == null) {
+                    System.out.println(" There are not enough coins to dispense the change, exiting the system");
+                    System.exit(0);
+                } else {
+                    System.out.printf("Coin Exchange For Amount $%d ==> %s ", amount, denomMap.entrySet()
+                            .stream()
+                            .map(Object::toString)
+                            .collect(joining(" & ")));
+                    System.out.println();
+                }
+                reader = new Scanner(System.in);  // Reading from System.in
+                System.out.println("Enter amount : ");
+                amount = reader.nextInt();
 
+            }
+        }catch (InputMismatchException ne) {
+            System.out.println("Enter only whole amount, ex: 10, 15,22");
+            System.exit(-1);
+        } finally {
+
+            reader.close();
         }
-        reader.close();
     }
 }
